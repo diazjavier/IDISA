539159988a0bb91452d2b2fbbc9970c9fa4af22d
diazjavier <diazjavier10@gmail.com>
2024-12-07 19:14:56 -0300

    Saco transition

--- src/components/PerfilCard.tsx
+++ src/components/PerfilCard.tsx
@@ -176,7 +176,6 @@ export default function PerfilCard(props: Readonly<PerfilProps>) {
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4">
             <DialogPanel
-              transition
               className="w-full max-w-md rounded-xl bg-white shadow-xl border duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
             >
               <img
